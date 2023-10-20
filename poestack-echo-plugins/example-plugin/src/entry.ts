5e1ab74eba2698b34737fb3c15137d61522a7ff0
zach <zgherridge@gmail.com>
2023-10-19 20:59:22 -0700

    working

--- poestack-echo-plugins/example-plugin/src/entry.ts
+++ poestack-echo-plugins/example-plugin/src/entry.ts
@@ -1,11 +1,4 @@
 import App from "./App.tsx";
 
-class ExamplePlugin {
-
-    public init(sageContext: any) {
-        sageContext.addComponent(App)
-    }
-}
-
 // @ts-ignore
-window.registerPlugin(new ExamplePlugin())
+window.registerString("asdas", App)
