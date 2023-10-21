2c51e2eb0efc0aa56efbeee56e08968eee9a599a
zach <zgherridge@gmail.com>
2023-10-21 01:42:11 -0700

    Setup test server

---
+++ poe-api-test-server/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const fs = require("fs");
+const path = require("path");
+const app = express()
+const port = 3000
+
+app.get('/*', (req, res) => {
+
+    const paths = ["poe-data", ...req.path.split("/").filter((e) => e.length > 0)]
+    const target = path.resolve(...paths) + ".json"
+    console.log("paths", target)
+    const body = fs.readFileSync(target).toString()
+    res.send(body)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
