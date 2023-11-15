005cb082bc2312cfbfcb3fd54019da4364daa2b0
Andrew Reder <avidreder@gmail.com>
2023-11-14 22:44:15 -0800

    add cors, move to dependencies

--- src/echo-app/src/local-server/server.ts
+++ src/echo-app/src/local-server/server.ts
@@ -1,11 +1,28 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import jwt from 'jsonwebtoken'
+import cors from 'cors'
 
 const EXPRESS_PORT = 8000
+const ALLOWED_ORIGINS = ['http://localhost:3000', 'https://poestack.com']
 
 export const createLocalServer = (messageSender: (token: string) => void) => {
   const expressServer = express()
+  expressServer.use(
+    cors({
+      origin: function (origin, callback) {
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true)
+        if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
+          var msg =
+            'The CORS policy for this site does not ' + 'allow access from the specified Origin.'
+          return callback(new Error(msg), false)
+        }
+        return callback(null, true)
+      }
+    })
+  )
   expressServer.use(bodyParser.json())
   expressServer.post('/auth', (req, res) => {
     res.setHeader('Content-Type', 'application/json')
