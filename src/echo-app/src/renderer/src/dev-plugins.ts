51f0cdcecf4d8caeb17969ffb680c6342773fc04
C3ntraX <nienhaus.andreas94@googlemail.com>
2023-11-15 21:37:48 +0100

    Cleanup devPlugins

--- src/echo-app/src/renderer/src/dev-plugins.ts
+++ src/echo-app/src/renderer/src/dev-plugins.ts
@@ -2,5 +2,4 @@
  * This plugins are used in dev mode - via 'npm run dev'
  * All of these plugins have HMR enabled, regardless of whether you only have the plugins folder open
  */
-
-export const DEV_PLUGINS = [import('character-plugin')]
+export const DEV_PLUGINS = []
