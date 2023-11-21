42d841a91781af49f0bb167e09a6ba4ced4a88f1
Andrew Reder <avidreder@gmail.com>
2023-11-20 20:11:55 -0800

    more infra

--- src/sage-aws-cdk/lib/sage-backend-stack.ts
+++ src/sage-aws-cdk/lib/sage-backend-stack.ts
@@ -1,5 +1,12 @@
 import * as cdk from 'aws-cdk-lib'
-import { aws_ecr, aws_ecs } from 'aws-cdk-lib'
+import {
+  CfnOutput,
+  aws_ecr,
+  aws_ecs,
+  aws_route53,
+  aws_certificatemanager,
+  RemovalPolicy
+} from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { DeploymentControllerType, EnvironmentFile, LogDriver } from 'aws-cdk-lib/aws-ecs'
 import { SageStack } from './sage-stack'
@@ -11,6 +18,23 @@ import { UpdatePolicy } from 'aws-cdk-lib/aws-autoscaling'
 export class SageBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps, sageStack: SageStack) {
     super(scope, id, props)
+
+    const domainName = 'poestack-sage.dev'
+    const siteDomain = 'www' + '.' + domainName
+
+    const hostedZone = new aws_route53.PublicHostedZone(this, 'MyHostedZone', {
+      zoneName: domainName
+    })
+
+    new CfnOutput(this, 'Site', { value: 'https://' + siteDomain })
+    const certificate = new aws_certificatemanager.Certificate(this, 'SageBackendCertificate', {
+      domainName: domainName,
+      subjectAlternativeNames: ['*.' + domainName],
+      validation: aws_certificatemanager.CertificateValidation.fromDns(hostedZone)
+    })
+
+    certificate.applyRemovalPolicy(RemovalPolicy.DESTROY)
+    new CfnOutput(this, 'Certificate', { value: certificate.certificateArn })
 
     const ecsCluster = new aws_ecs.Cluster(this, 'SageBackendCluster', {
       clusterName: 'sage-backend-cluster',
@@ -39,16 +63,54 @@ export class SageBackendStack extends cdk.Stack {
         logRetention: RetentionDays.THREE_DAYS
       }),
       command: ['node', 'src/sage-backend/dist/index.js'],
-      environmentFiles: [EnvironmentFile.fromBucket(sageStack.configBucket, 'sage-backend.env')]
+      environmentFiles: [EnvironmentFile.fromBucket(sageStack.configBucket, 'sage-backend.env')],
+      portMappings: [
+        {
+          containerPort: 9000,
+          protocol: aws_ecs.Protocol.TCP
+        }
+      ]
     })
     sageStack.configBucket.grantRead(sageBackendTask.executionRole!!)
-    new aws_ecs.Ec2Service(this, 'InsStreamConsumerSvc', {
+    const sageBackendService = new aws_ecs.Ec2Service(this, 'SageBackendSvc', {
       cluster: ecsCluster,
       taskDefinition: sageBackendTask,
       deploymentController: {
         type: DeploymentControllerType.ECS
       },
       circuitBreaker: undefined
+    })
+
+    const sageBackendAlb = new cdk.aws_elasticloadbalancingv2.ApplicationLoadBalancer(
+      this,
+      'SageBackendAlb',
+      {
+        vpc: sageStack.vpc,
+        internetFacing: true
+      }
+    )
+
+    const albListener = sageBackendAlb.addListener('SageBackendALBHttpsListener', {
+      port: 443,
+      certificates: [certificate]
+    })
+
+    albListener.addTargets('SageBackendServiceTarget', {
+      port: 443,
+      targets: [
+        sageBackendService.loadBalancerTarget({
+          containerName: 'SageBackend',
+          containerPort: 9000
+        })
+      ]
+    })
+
+    new aws_route53.ARecord(this, 'WWWSiteAliasRecord', {
+      zone: hostedZone,
+      recordName: siteDomain,
+      target: aws_route53.RecordTarget.fromAlias(
+        new cdk.aws_route53_targets.LoadBalancerTarget(sageBackendAlb)
+      )
     })
   }
 }
