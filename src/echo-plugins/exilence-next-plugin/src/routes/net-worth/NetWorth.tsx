edf92ccc06cf715e7b23ff8a9f4651e417228c72
Andrew Reder <avidreder@gmail.com>
2023-11-22 15:26:06 -0800

    toolbar skeleton

--- src/echo-plugins/exilence-next-plugin/src/routes/net-worth/NetWorth.tsx
+++ src/echo-plugins/exilence-next-plugin/src/routes/net-worth/NetWorth.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { observer } from 'mobx-react'
 import { useStore } from '../../hooks/useStore'
+import { Toolbar } from '../../components/Toolbar/Toolbar'
 
 const NetWorth = () => {
   const store = useStore()
 
   return (
-    <>
-      {/* <button
+    <div className="relative flex flex-col h-full w-full">
+      <Toolbar />
+      <main className="flex-row p-2">
+        {/* <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1"
         onClick={() => {
           uiStateStore?.increment()
@@ -15,65 +18,65 @@ const NetWorth = () => {
       >
         Increment
       </button> */}
-      <br />
-      <button
-        className="h-10 px-6 font-semibold rounded-full bg-violet-600 text-white mb-1"
-        onClick={() => {
-          store?.uiStateStore.addAccount()
-        }}
-      >
-        addAccount
-      </button>
-      <br />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1"
-        onClick={() => {
-          store?.uiStateStore.addAccountToActiveAccountStore()
-        }}
-      >
-        addAccountToActiveAccountStore
-      </button>
-      <br />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1"
-        onClick={() => {
-          store?.uiStateStore.addProfileToActiveAccount()
-        }}
-      >
-        addProfileToActiveAccount
-      </button>
-      <br />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1"
-        onClick={() => {
-          store?.accountStore.initSession()
-        }}
-      >
-        InitSession
-      </button>
-      <br />
-      Accounts:
-      {store?.accountStore.accounts.map((acccount) => {
-        return (
-          <ul>
-            <li key={acccount.name}>{acccount!.name}</li>
-          </ul>
-        )
-      })}
-      <br />
-      Active account: {store?.accountStore.activeAccount?.name}
-      <br />
-      Profiles in account: {store?.accountStore.activeAccount?.name}
-      {store?.accountStore.activeAccount?.profiles.map((profile) => {
-        return (
-          <ul>
-            <li key={profile.name}>{profile.name}</li>
-          </ul>
-        )
-      })}
-      <br />
-      Active profile: {store?.accountStore.activeAccount?.activeProfile?.name}
-      {/* <button
+        <br />
+        <button
+          className="h-10 px-6 font-semibold rounded-full bg-violet-600 text-white mb-1"
+          onClick={() => {
+            store?.uiStateStore.addAccount()
+          }}
+        >
+          addAccount
+        </button>
+        <br />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1"
+          onClick={() => {
+            store?.uiStateStore.addAccountToActiveAccountStore()
+          }}
+        >
+          addAccountToActiveAccountStore
+        </button>
+        <br />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1"
+          onClick={() => {
+            store?.uiStateStore.addProfileToActiveAccount()
+          }}
+        >
+          addProfileToActiveAccount
+        </button>
+        <br />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1"
+          onClick={() => {
+            store?.accountStore.initSession()
+          }}
+        >
+          InitSession
+        </button>
+        <br />
+        Accounts:
+        {store?.accountStore.accounts.map((acccount) => {
+          return (
+            <ul>
+              <li key={acccount.name}>{acccount!.name}</li>
+            </ul>
+          )
+        })}
+        <br />
+        Active account: {store?.accountStore.activeAccount?.name}
+        <br />
+        Profiles in account: {store?.accountStore.activeAccount?.name}
+        {store?.accountStore.activeAccount?.profiles.map((profile) => {
+          return (
+            <ul>
+              <li key={profile.name}>{profile.name}</li>
+            </ul>
+          )
+        })}
+        <br />
+        Active profile: {store?.accountStore.activeAccount?.activeProfile?.name}
+        {/* <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-1"
         onClick={() => {
           uiStateStore?.subscribtion()
@@ -81,8 +84,9 @@ const NetWorth = () => {
       >
         Subscribtion increment
       </button> */}
-      {/* <div>{uiStateStore?.counter}</div> */}
-    </>
+        {/* <div>{uiStateStore?.counter}</div> */}
+      </main>
+    </div>
   )
 }
 
