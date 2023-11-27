054d77a577ff42d8802489fff0b03a63f632a19f
C3ntraX <nienhaus.andreas94@googlemail.com>
2023-11-27 18:45:42 +0100

    Add shadcn components and rework exports resolution
    (cherry picked from commit fdc5559dc68950b58476ce4bd49307bea110cd55)

--- src/echo-app/tailwind.config.js
+++ src/echo-app/tailwind.config.js
@@ -1,4 +1,4 @@
-const { fontFamily } = require("tailwindcss/defaultTheme")
+const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
@@ -11,78 +11,92 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
-      },
+        '2xl': '1400px'
+      }
     },
     extend: {
       colors: {
-        'primary-surface': 'var(--color-primary-surface)', //Deprecated
-        'input-surface': 'var(--color-input-surface)', //Deprecated
-        'secondary-surface': 'var(--color-secondary-surface)', //Deprecated
-        'primary-text': 'var(--color-text)', //Deprecated
-        'primary-accent': 'var(--color-primary-accent)', //Deprecated
+        /**
+         * Deprecated
+         */
+        'primary-surface': 'var(--color-primary-surface)',
+        /**
+         * Deprecated
+         */
+        'input-surface': 'var(--color-input-surface)',
+        /**
+         * Deprecated
+         */
+        'secondary-surface': 'var(--color-secondary-surface)',
+        /**
+         * Deprecated
+         */
+        'primary-text': 'var(--color-text)',
+        /**
+         * Deprecated
+         */
+        'primary-accent': 'var(--color-primary-accent)',
 
-        gradient: "var(--gradient)",
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        gradient: 'var(--gradient)',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
         },
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
         },
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        }
       },
       borderRadius: {
         lg: `var(--radius)`,
         md: `calc(var(--radius) - 2px)`,
-        sm: "calc(var(--radius) - 4px)",
+        sm: 'calc(var(--radius) - 4px)'
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ['var(--font-sans)', ...fontFamily.sans]
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        }
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
+      }
     }
-
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')]
 }
--- src/echo-common/src/index.ts
+++ src/echo-common/src/index.ts
@@ -1,44 +1,15 @@
-import { EchoRoute, EchoRouter } from './echo-router'
-import { SmartCache, SmartCacheEvent } from './smart-cache'
-import { EchoPluginHook } from './echo-plugin-hook'
-import { PoeAccountService } from './poe-account-service'
-import { PoeCharacterService } from './poe-character-service'
-import { EchoPoeItem, PoeStashService } from './poe-stash-service'
-import { PoeClientLogService, PoeZoneEntranceEvent } from './poe-client-log-service'
-import { EchoPluginConfigs, EchoPluginConfig } from './echo-plugin-config'
-import { EchoDirService } from './echo-dir-service'
-import { EchoContext } from './echo-context'
-import { EchoPluginService } from './echo-plugin-service'
-import { ECHO_CONTEXT_SERVICE, EchoContextService } from './echo-context-service'
-import { SageValuationService } from './sage-valuation-service'
-import { validResults } from './smart-cache-hooks'
-import { ActionTooltip } from './ui/components/v1/action-tooltip'
-import { cn } from './ui/lib/utils'
-
-export {
-  PoeStashService,
-  SageValuationService,
-  SmartCache,
-  EchoPluginService,
-  PoeAccountService,
-  PoeCharacterService,
-  PoeClientLogService,
-  EchoRouter,
-  EchoDirService,
-  validResults,
-  EchoContextService,
-  ECHO_CONTEXT_SERVICE,
-  ActionTooltip,
-  cn
-}
-
-export type {
-  EchoPluginHook,
-  SmartCacheEvent,
-  EchoPoeItem,
-  PoeZoneEntranceEvent,
-  EchoPluginConfigs,
-  EchoPluginConfig,
-  EchoRoute,
-  EchoContext
-}
+export * from './echo-router'
+export * from './smart-cache'
+export * from './echo-plugin-hook'
+export * from './poe-account-service'
+export * from './poe-character-service'
+export * from './poe-stash-service'
+export * from './poe-client-log-service'
+export * from './echo-plugin-config'
+export * from './echo-dir-service'
+export * from './echo-context'
+export * from './echo-plugin-service'
+export * from './echo-context-service'
+export * from './sage-valuation-service'
+export * from './smart-cache-hooks'
+export * from './ui/lib/utils'
