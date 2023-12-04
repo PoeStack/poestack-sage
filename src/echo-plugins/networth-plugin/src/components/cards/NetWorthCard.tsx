b40affe8a9eba4dcf57670dd5ea7248524832214
Andrew Reder <avidreder@gmail.com>
2023-12-04 11:14:25 -0800

    card styles, collapsing

--- src/echo-plugins/networth-plugin/src/components/cards/NetWorthCard.tsx
+++ src/echo-plugins/networth-plugin/src/components/cards/NetWorthCard.tsx
@@ -3,13 +3,14 @@
 import { useRef, useState } from 'react'
 import * as Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
-import { ChevronDown } from 'lucide-react'
+import { ChevronDownIcon, ChevronRightIcon } from 'lucide-react'
 
-import { DropdownMenu, Button, Card } from 'echo-common/components-v1'
+import { Collapsible, Card } from 'echo-common/components-v1'
 import { observer } from 'mobx-react'
 import { useStore } from '../../hooks/useStore'
 
 function NetWorthCard() {
+  const [open, setOpen] = useState(false)
   const options: Highcharts.Options = {
     series: [
       {
@@ -24,9 +25,9 @@ function NetWorthCard() {
             y2: 1
           },
           stops: [
-            [0, 'hsla(var(--muted), 0.1)'],
-            [0, 'hsla(var(--muted), 0.5)'],
-            [1, 'hsla(var(--muted), 0.8)']
+            [0, 'hsla(var(--muted), 1)'],
+            [0.5, 'hsla(var(--muted), 0.5)'],
+            [1, 'hsla(var(--muted), 0.2)']
           ]
         },
         marker: {
@@ -39,6 +40,7 @@ function NetWorthCard() {
       enabled: false
     },
     chart: {
+      height: 200,
       backgroundColor: 'hsl(var(--card))'
     },
     yAxis: {
@@ -109,16 +111,27 @@ function NetWorthCard() {
   const chartComponentRef = useRef<HighchartsReact.RefObject>(null)
 
   return (
-    <Card>
-      <Card.Header>
-        <Card.Title>Net Worth History Chart</Card.Title>
-      </Card.Header>
-      <Card.Content>
-        <div className="p-2">
-          <HighchartsReact highcharts={Highcharts} options={options} ref={chartComponentRef} />
-        </div>
-      </Card.Content>
-    </Card>
+    <Collapsible open={open} onOpenChange={setOpen}>
+      <Card>
+        <Card.Header className="flex flex-row justify-between items-center p-3">
+          <Card.Title>Net Worth History Chart</Card.Title>
+          <Collapsible.Trigger className="!mt-0" asChild>
+            {open ? (
+              <ChevronDownIcon className="h-4 w-4" />
+            ) : (
+              <ChevronRightIcon className="h-4 w-4" />
+            )}
+          </Collapsible.Trigger>
+        </Card.Header>
+        <Collapsible.Content>
+          <Card.Content className="p-2">
+            <div className="px-2">
+              <HighchartsReact highcharts={Highcharts} options={options} ref={chartComponentRef} />
+            </div>
+          </Card.Content>
+        </Collapsible.Content>
+      </Card>
+    </Collapsible>
   )
 }
 
