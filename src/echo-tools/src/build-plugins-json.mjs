32c9986ead20475c7b32e95c1a077cff448dd9fc
Andrew Reder <avidreder@gmail.com>
2023-12-07 14:34:34 -0800

    update plugin build script

--- src/echo-tools/src/build-plugins-json.mjs
+++ src/echo-tools/src/build-plugins-json.mjs
@@ -1,30 +1,53 @@
-import path from 'path'
-import fs from "fs"
+import path from "path";
+import fs from "fs";
 
-const publicPath = path.resolve("..", "..", "public")
+const publicPath = path.resolve("..", "..", "public");
 if (!fs.existsSync(publicPath)) {
-  fs.mkdirSync(publicPath)
+  fs.mkdirSync(publicPath);
 }
 
-const pluginsJsonPath = path.resolve(publicPath, "plugins.json")
+const pluginsJsonPath = path.resolve(publicPath, "plugins.json");
 if (!fs.existsSync(pluginsJsonPath)) {
-  fs.writeFileSync(pluginsJsonPath, "{}")
+  fs.writeFileSync(pluginsJsonPath, "{}");
 }
 
-const baseDir = path.resolve("..", "echo-plugin-examples")
-const echoCommonVersion = JSON.parse(fs.readFileSync(path.resolve("..", "echo-common", "package.json"))).version
+const examplePluginsDir = path.resolve("..", "echo-plugin-examples");
+const pluginsDir = path.resolve("..", "echo-plugins");
+const echoCommonVersion = JSON.parse(
+  fs.readFileSync(path.resolve("..", "echo-common", "package.json"))
+).version;
 
-const plugins = JSON.parse(fs.readFileSync(pluginsJsonPath))
-const files = fs.readdirSync(baseDir)
-files.forEach(function(file) {
-  const currentPackage = JSON.parse(fs.readFileSync(path.resolve(baseDir, file, "package.json")))
-  console.log("asdasd", currentPackage)
-  const lastPlugin = plugins[currentPackage.name]
+const manifestPlugins = JSON.parse(fs.readFileSync(pluginsJsonPath));
+const examplePlugins = fs.readdirSync(examplePluginsDir);
+const plugins = fs.readdirSync(pluginsDir);
+examplePlugins.forEach(function (file) {
+  const currentPackage = JSON.parse(
+    fs.readFileSync(path.resolve(examplePluginsDir, file, "package.json"))
+  );
+  const lastPlugin = manifestPlugins[currentPackage.name];
 
   if (lastPlugin?.version !== currentPackage.version) {
-    plugins[currentPackage.name] = { name: currentPackage.name, version: currentPackage.version, echoCommonVersion: echoCommonVersion }
+    manifestPlugins[currentPackage.name] = {
+      name: currentPackage.name,
+      version: currentPackage.version,
+      echoCommonVersion: echoCommonVersion,
+    };
   }
-})
+});
+plugins.forEach(function (file) {
+  const currentPackage = JSON.parse(
+    fs.readFileSync(path.resolve(pluginsDir, file, "package.json"))
+  );
+  const lastPlugin = manifestPlugins[currentPackage.name];
 
-console.log("writing", plugins)
-fs.writeFileSync(pluginsJsonPath, JSON.stringify(plugins, null, 2))
+  if (lastPlugin?.version !== currentPackage.version) {
+    manifestPlugins[currentPackage.name] = {
+      name: currentPackage.name,
+      version: currentPackage.version,
+      echoCommonVersion: echoCommonVersion,
+    };
+  }
+});
+
+console.log("writing", manifestPlugins);
+fs.writeFileSync(pluginsJsonPath, JSON.stringify(manifestPlugins, null, 2));
