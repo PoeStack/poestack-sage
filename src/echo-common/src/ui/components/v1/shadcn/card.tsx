35c54e0544b59cfeae059ad758c58fd675dabb1a
C3ntraX <nienhaus.andreas94@googlemail.com>
2023-12-29 12:02:21 +0100

    Revert new-york style to default style

--- src/echo-common/src/ui/components/v1/shadcn/card.tsx
+++ src/echo-common/src/ui/components/v1/shadcn/card.tsx
@@ -6,7 +6,7 @@ const CardRoot = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivEl
   ({ className, ...props }, ref) => (
     <div
       ref={ref}
-      className={cn('rounded-xl border bg-card text-card-foreground shadow', className)}
+      className={cn('rounded-lg border bg-card text-card-foreground shadow-sm', className)}
       {...props}
     />
   )
@@ -24,7 +24,7 @@ const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HT
   ({ className, ...props }, ref) => (
     <h3
       ref={ref}
-      className={cn('font-semibold leading-none tracking-tight', className)}
+      className={cn('text-2xl font-semibold leading-none tracking-tight', className)}
       {...props}
     />
   )
